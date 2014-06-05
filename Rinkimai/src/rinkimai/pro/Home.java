ee6b80718841c9f53093780a562ed04b6e100909
arn8tas@inbox.lt <arn8tas@inbox.lt>
2014-06-05 21:34:16 +0300

    ghg

--- Rinkimai/src/rinkimai/pro/Home.java
+++ Rinkimai/src/rinkimai/pro/Home.java
@@ -28,7 +28,7 @@ public class Home extends Activity implements OnClickListener
 {
 	private EditText email, pass;
 	private Button mSubmit, mRegister;
-//testas 
+//testas 1
 	// Progress Dialog
 	private ProgressDialog pDialog;
 

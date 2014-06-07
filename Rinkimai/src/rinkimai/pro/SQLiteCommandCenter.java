86c9f310e1227eb042f5e3b2e52cfe468a279f59
jonas111 <aronax225@gmail.com>
2014-06-07 19:02:16 +0200

    Update SQLiteCommandCenter.java
    bandom

--- Rinkimai/src/rinkimai/pro/SQLiteCommandCenter.java
+++ Rinkimai/src/rinkimai/pro/SQLiteCommandCenter.java
@@ -35,7 +35,6 @@ public class SQLiteCommandCenter extends SQLiteOpenHelper
 		db = createdbIfNotExists();
 		createTables(db);
 		
-		NetworkStatusListener nl = new NetworkStatusListener();
 		
 
 		checkBalsavimai();
@@ -197,24 +196,6 @@ public class SQLiteCommandCenter extends SQLiteOpenHelper
 		
 	}
 	
-//	public class NetworkStatusListener extends BroadcastReceiver
-//	{
-//	  @Override
-//	  public void onReceive( Context context, Intent intent )
-//	  {
-//	    ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService( Context.CONNECTIVITY_SERVICE );
-//	    NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
-//	    NetworkInfo mobNetInfo = connectivityManager.getNetworkInfo(     ConnectivityManager.TYPE_MOBILE );
-//	    if ( activeNetInfo != null )
-//	    {
-//	      Toast.makeText( context, "Active Network Type : " + activeNetInfo.getTypeName(), Toast.LENGTH_SHORT ).show();
-//	    }
-//	    if( mobNetInfo != null )
-//	    {
-//	      Toast.makeText( context, "Mobile Network Type : " + mobNetInfo.getTypeName(), Toast.LENGTH_SHORT ).show();
-//	    }
-//	  }
-//	}
 	
 
 }

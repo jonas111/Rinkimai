754116dc9ceddd67e4bc76a3f7dc61581c53b8aa
jonas111 <aronax225@gmail.com>
2014-06-07 18:56:45 +0200

    Update SQLiteCommandCenter.java
    sutaisiau Arturai

--- Rinkimai/src/rinkimai/pro/SQLiteCommandCenter.java
+++ Rinkimai/src/rinkimai/pro/SQLiteCommandCenter.java
@@ -197,24 +197,24 @@ public class SQLiteCommandCenter extends SQLiteOpenHelper
 		
 	}
 	
-	public class NetworkStatusListener extends BroadcastReceiver
-	{
-	  @Override
-	  public void onReceive( Context context, Intent intent )
-	  {
-	    ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService( Context.CONNECTIVITY_SERVICE );
-	    NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
-	    NetworkInfo mobNetInfo = connectivityManager.getNetworkInfo(     ConnectivityManager.TYPE_MOBILE );
-	    if ( activeNetInfo != null )
-	    {
-	      Toast.makeText( context, "Active Network Type : " + activeNetInfo.getTypeName(), Toast.LENGTH_SHORT ).show();
-	    }
-	    if( mobNetInfo != null )
-	    {
-	      Toast.makeText( context, "Mobile Network Type : " + mobNetInfo.getTypeName(), Toast.LENGTH_SHORT ).show();
-	    }
-	  }
-	}
+//	public class NetworkStatusListener extends BroadcastReceiver
+//	{
+//	  @Override
+//	  public void onReceive( Context context, Intent intent )
+//	  {
+//	    ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService( Context.CONNECTIVITY_SERVICE );
+//	    NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+//	    NetworkInfo mobNetInfo = connectivityManager.getNetworkInfo(     ConnectivityManager.TYPE_MOBILE );
+//	    if ( activeNetInfo != null )
+//	    {
+//	      Toast.makeText( context, "Active Network Type : " + activeNetInfo.getTypeName(), Toast.LENGTH_SHORT ).show();
+//	    }
+//	    if( mobNetInfo != null )
+//	    {
+//	      Toast.makeText( context, "Mobile Network Type : " + mobNetInfo.getTypeName(), Toast.LENGTH_SHORT ).show();
+//	    }
+//	  }
+//	}
 	
 
 }
